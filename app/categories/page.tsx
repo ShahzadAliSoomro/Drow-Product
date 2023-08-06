3056e10c9e17a86888ccc1d614350ff10a604228
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-08-07 02:00:30 +0500

    continue

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -157,73 +157,73 @@ const MaleCard = [
   const KidzCard = [
     {
       id: 1,
-      image: "/images/m10.jpg",
+      image: "/images/k1.jpg",
       title: "Eto V Neck Yellow",
       price: "Rp. 129.000",
     },
     {
       id: 2,
-      image: "/images/m-11.jpg",
+      image: "/images/k3.jpg",
       title: "Macaroon Dry Half",
       price: "Rp. 139.000",
     },
     {
       id: 3,
-      image: "/images/m-12.jpg",
+      image: "/images/k2.jpg",
       title: "Wave Stripe Hally",
       price: "Rp. 130.000",
     },
     {
       id: 4,
-      image: "/images/m-13.jpg",
+      image: "/images/k4.jpg",
       title: "Eve Punching Floral",
       price: "Rp. 229.000",
     },
     {
       id: 5,
-      image: "/images/m-16.jpg",
+      image: "/images/k5.jpg",
       title: "Floral Waffle Tee",
       price: "Rp. 119.000",
     },
     {
       id: 6,
-      image: "/images/male6.jpg",
+      image: "/images/k6.jpg",
       title: "Snap Pure Blouse",
       price: "Rp. 225.000",
     },
     {
         id: 7,
-        image: "/images/male6.jpg",
+        image: "/images/k7.jpg",
         title: "Euphoria Crop Blouse",
         price: "Rp. 125.000",
       },
       {
         id: 8,
-        image: "/images/male9.jpg",
+        image: "/images/k8.jpg",
         title: "Venus Halter Dress",
         price: "Rp. 180.000",
       },
       {
         id: 9,
-        image: "/images/m-15.jpg",
+        image: "/images/k9.jpg",
         title: "Snap Pure Blouse",
         price: "Rp. 129.000",
       },
       {
         id: 10,
-        image: "/images/m-14.jpg",
+        image: "/images/k10.jpg",
         title: "West Bootscut Denim",
         price: "Rp. 229.000",
       },
       {
         id: 11,
-        image: "/images/male8.jpg",
+        image: "/images/k11.jpg",
         title: "Ribbon Check Long",
         price: "Rp. 175.000",
       },
       {
         id: 12,
-        image: "/images/m-17.jpg",
+        image: "/images/k12.jpg",
         title: "Shouldcut Knit",
         price: "Rp. 149.000",
       },
@@ -277,6 +277,26 @@ export default function Page() {
         </div>
         <div className="lg:w-[1240px] w-full mx-auto grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
           {MaleCard.map((item) => (
+            <div className="flex gap-4 flex-col">
+              <Image
+                src={item.image}
+                alt="5Image"
+                width={400}
+                height={400}
+                className="h-full"
+              />
+              <div className="flex justify-center flex-col items-center text-lg text-[#404040]">
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center items-center lg:text-5xl text-3xl font-bold text-[#404040]">
+          <h2>Season Collections Kids</h2>
+        </div>
+        <div className="lg:w-[1240px] w-full mx-auto grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
+          {KidzCard.map((item) => (
             <div className="flex gap-4 flex-col">
               <Image
                 src={item.image}
