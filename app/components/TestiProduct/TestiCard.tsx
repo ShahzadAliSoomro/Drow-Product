6677a11533f6a0947b57a22898cadcde1996651d
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-08-14 17:10:26 +0500

    changing

--- app/components/TestiProduct/TestiCard.tsx
+++ app/components/TestiProduct/TestiCard.tsx
@@ -105,10 +105,10 @@ const TestiCard = () => {
         <div className="flex justify-center items-center lg:text-5xl text-4xl font-bold text-[#404040]">
           <h2>What They Said</h2>
         </div>
-        <div className="lg:w-[1240px] mx-auto ">
+        <div className="lg:w-[1240px] mx-auto">
         <Slider {...settings}>
           {CardData.map((data) => (
-            <div key={data.id} className="p-2">
+            <div key={data.id} className="">
               <div className="flex justify-center">
                 <div className="bg-[#F7F6F0] p-2 rounded-lg shadow-md hover:-translate-y-3 transition-all duration-500">
                   <Image
